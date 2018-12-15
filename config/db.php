c4d03131a0d2deb64bc733dac176524100099535
mesiyar <1021683438@qq.com>
2018-12-15 15:05:43 +0800

    Update db.php
    modify config

--- config/db.php
+++ config/db.php
@@ -6,9 +6,9 @@
  * Time: 下午5:14
  */
 return [
-    'host' => '192.168.1.21',
+    'host' => 'xxxxx',
     'port' => '3306',
     'user' => 'root',
-    'passwd' => 'Bdsk@123',
+    'passwd' => 'xxxxx',
     'dbname' => 'gm',
 ];
